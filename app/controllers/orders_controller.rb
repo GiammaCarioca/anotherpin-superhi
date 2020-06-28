afd3a4eaeccbb5a7345462a58f987c4f5fff9395
Thiago Giammattey <giammattey@gmail.com>
2020-06-28 18:16:44 -0300

    new orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,20 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def create
+    @order = Order.new(form_params)
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render "new"
+    end
+
+  end
+
+  def form_params
+    params.require(:order).permit(:first_name, :last_name, :email,
+      :country, :address_1, :address_2, :city, :postal_code)
+  end
+
 end
