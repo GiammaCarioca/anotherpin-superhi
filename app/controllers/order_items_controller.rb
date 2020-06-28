37904f3d385e6f4e77ef5d4bd91b8bc683ad342c
Thiago Giammattey <giammattey@gmail.com>
2020-06-28 13:08:35 -0300

    adding a cart controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,19 @@
 class OrderItemsController < ApplicationController
+
+  def create
+    # find the product
+    @product = Product.find(params[:product_id])
+
+    # quantity? – comes from the form data
+    @quantity = form_params[:quantity]
+
+    @current_cart.order_items.create(product: @product, quantity: @quantity)
+
+    redirect_to product_path(@product)
+  end
+
+  def form_params
+    params.require(:order_item).permit(:quantity)
+  end
+
 end
