de0e825c1349f4e9ca4402ffb294b15bbfff6693
Thiago Giammattey <giammattey@gmail.com>
2020-06-29 14:13:46 -0300

    adding Stripe integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_183255) do
+ActiveRecord::Schema.define(version: 2020_06_29_141443) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_183255) do
     t.string "postal_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_token"
   end
 
   create_table "products", force: :cascade do |t|
